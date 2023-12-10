fbacdcc1b0650849e66b198b6613b67c518f7d81
Sergiyss <weert19933@gmail.com>
2023-12-10 20:04:52 +0200

    Подготовка к многомодульному проекту

---
+++ profile/src/main/java/ua/dev/webnauts/profile/ProfileScreen.kt
@@ -0,0 +1,11 @@
+package ua.dev.webnauts.profile
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProfileScreen() {
+    Text("Hello User",
+        modifier = Modifier)
+}
